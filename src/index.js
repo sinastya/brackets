ece6fb3c07ad24dd327f7be9843f394241b21c47
sinastya <sinastya@yandex.ru>
2022-07-20 20:12:14 +0300

    kinda done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let conf =  bracketsConfig.map(el => el.join(''));
+    for (let i=0; i<30; i++) {
+        for (br of conf) {
+            let str_new = str;
+            if (br == '()' || br == '[]' || br == '||') {
+                regex = new RegExp(`\\${br[0]}\\${br[1]}`, 'g');
+                str = str_new.replace(regex, '');
+            } else {
+                let regex = new RegExp(`${br}`, 'g');
+                str = str_new.replace(regex, '');
+            }
+        }   
+    }
+    return (str.length == 0)
 }
